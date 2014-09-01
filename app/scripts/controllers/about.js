a59ff93e7c74399d688a42219cf1894bd2d768d1
Peter Hildenbrand <p.hildenbrand@bitexpert.de>
2014-09-01 11:38:48 +0200

    Initial project setup using yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bitDeveloperDay2014App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bitDeveloperDay2014App
+ */
+angular.module('bitDeveloperDay2014App')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bitDeveloperDay2014App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bitDeveloperDay2014App
+ */
+angular.module('bitDeveloperDay2014App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
